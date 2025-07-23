3ddc2d0ffcc25da850f7309d10fe3cbfcfcceaaf
Anuvind-ap <anuvindap2016@gmail.com>
2025-07-24 00:25:35 +0530

    doesn't support bluetooth 5.1 devices but works for other

--- main.sh
+++ main.sh
@@ -4,7 +4,23 @@ usage() {
     echo "add it later!!!!!!!!!"
 }
 
-while getopts "hd:" opt; do 
+normalize_mac() {
+    local input="$1"
+    echo "$input" | tr -d ':' | tr 'A-F' 'a-f'
+}
+
+extract_hex_bytes() {
+    local input="$1"
+    echo "$input" | grep '^:00000' | cut -c9-56 | tr -d ' '
+}
+
+change_linkkey() {
+    local file="$1"
+    local new_key="$2"
+    sed -i "/^\[LinkKey\]/,/^\[/{s/^Key=.*/Key=$new_key/}" "$file"
+}
+
+while getopts "hd:m:" opt; do 
     case $opt in
         h) 
             usage
@@ -13,7 +29,9 @@ while getopts "hd:" opt; do
         d) 
             DISK="$OPTARG"
             ;;
-
+        m)
+            TARGETDEVICE="$OPTARG"
+            ;;
     esac
 done
 
@@ -23,8 +41,8 @@ if (( $EUID!=0 )); then
 fi
 echo -e "running in root privileges...\n"
 
-if [ -z "$DISK" ]; then
-    echo "missing -d <arg>"
+if [ -z "$DISK" ] || [ -z "$TARGETDEVICE" ]; then
+    echo "missing -d <arg> and -m <arg>"
     usage
     exit 1
 fi
@@ -41,15 +59,19 @@ controlset_num=$(echo "$outputCS" | grep '<Current>' | awk '{print $(NF-1)}')
 printf -v cs "ControlSet%03d" "$controlset_num"
 echo -e "accessing $cs\n"
 
-outputbtdriver=$(echo -e "cd $cs\Services\BTHPORT\Parameters\Keys\nls\nq" | chntpw -e ~/SYSTEM.copy)
-btdriver=$(echo "$outputbtdriver" | awk 'NR==12 {print $NF}' | tr -d '<>')
-echo -e "accessing $cs\Services\BTHPORT\Parameters\Keys\\$btdriver as bluetooth controller\n"
+btdriverlinux=$(ls /var/lib/bluetooth/)
+btdriverwindows=$(normalize_mac "$btdriverlinux")
 
+echo -e "accessing $cs\Services\BTHPORT\Parameters\Keys\\$btdriverwindows as bluetooth controller\n"
 
+targetdevicewindows=$(normalize_mac "$TARGETDEVICE")
+outputhex=$(echo -e "cd $cs\Services\BTHPORT\Parameters\Keys\\$btdriverwindows\nhex $targetdevicewindows\nq" | chntpw -e ~/SYSTEM.copy)
+hex=$(extract_hex_bytes "$outputhex" )
+echo -e "hex key for target device: $hex\n"
 
-# controlset_num=$((16#controlset_hex))
-# echo "$controlset_num"
+change_linkkey "/var/lib/bluetooth/$btdriverlinux/$TARGETDEVICE/info" "$hex"
+echo  -e "updated the Key in the info file of Target Device $TARGETDEVICE\n"
+echo -e "PLEASE RESTART BLUETOOTH SERVICE TO CONNECT THE DEVICE, USE THE BELOW COMMAND TO RESTART...\nsystemctl restart bluetooth"
 
-echo "\n\n"
-exit 1
+exit 0
 
